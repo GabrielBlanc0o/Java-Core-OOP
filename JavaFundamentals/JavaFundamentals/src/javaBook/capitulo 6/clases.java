7fa659723ca2f872560dd304b4cb6b7497db93bc
GabrielBlanc0o <gtbvacation@outlook.es>
2026-05-21 10:56:58 -0500

    sobre clases y metodos

---
+++ JavaFundamentals/JavaFundamentals/src/javaBook/capitulo 6/clases.java
@@ -0,0 +1,25 @@
+
+// forma general de una clase en Java
+
+public class clases {
+    
+    class classname {
+
+        type instancia-variable1;
+        type instancia-variable2;
+        type instancia-variableN;
+
+        type metodo1(parametro-lista){
+            // cuerpo del metodo
+        }
+        
+        type metodo2(parametro-lista){
+            // cuerpo del metodo
+        }
+
+        type metodoN(parametro-lista){
+            // cuerpo del metodo
+        }s
+    }
+
+}

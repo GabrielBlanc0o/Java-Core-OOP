bf63ae9f37c3e8b65110a7ef9a5587e0895d0635
GabrielBlanc0o <gtbvacation@outlook.es>
2026-05-27 18:27:11 -0500

    Repasando el core de java con el libro desde 0 para reforzar rapidamente.

---
+++ 01-Fundamentals/src/Libro13th/Example2.java
@@ -0,0 +1,13 @@
+
+
+public class Example2 {
+    public static void main(String[] args){
+
+        int num;
+        num = 100;
+        System.out.println("Este es el numero: " + num);
+        num = num * 2;
+        System.out.print("El valor de num * 2 es ");
+        System.out.println(num);
+    }
+}
---
+++ 01-Fundamentals/src/Libro13th/IfSample.java
@@ -0,0 +1,21 @@
+
+public class IfSample {
+    public static void main(String[] args) {
+        int x,y;
+         
+        x = 10;
+        y = 20;
+
+        if (x > y) System.out.println("x es menos que y"); 
+
+        x = x * 2;
+        if (x ==y) System.out.println(" x ahora es igual a y");
+
+        x = x * 2;
+        if (x > y) System.out.println(" x es mas que y");
+
+        if (x == y) System.out.println(" esto no se muestra");
+
+    }
+    
+}

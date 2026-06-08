10c9275523f4bc3214e9731c2aeb2a03b012d27d
GabrielBlanc0o <gtbvacation@outlook.es>
2026-06-08 18:01:26 -0500

    feat: implement overloaded methods for mathematical operations and demonstrate runtime polymorphism with Animal subclasses

---
+++ 02-Object-Oriented/src/POO_ibm/Polimorfismo/RuntimePoly.java
@@ -0,0 +1,41 @@
+// ocurre cuando una subclase nos da implementacion 
+// especifica de un metodo definido en una superclase
+
+class Animal
+{
+    void sonido()
+    {
+        System.out.println("Animal hace un sonido.");    
+    }
+}
+
+class Perro extends Animal
+{
+    @Override
+    void sonido()
+    {
+        System.out.println("Perro ladra");
+    }
+}
+
+class Gato extends Animal
+{
+    @Override
+    void sonido()
+    {
+        System.out.println("Gato Miau");
+    }
+}
+
+public class RuntimePoly
+{
+    public static void main(String[] args) 
+    {
+        Animal miAnimal; // Declaramos una referencia Animal
+        miAnimal = new Perro();
+        miAnimal.sonido();
+
+        miAnimal = new Gato();
+        miAnimal.sonido();
+    }
+}

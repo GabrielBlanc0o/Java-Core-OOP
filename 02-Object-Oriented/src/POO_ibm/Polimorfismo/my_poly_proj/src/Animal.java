02c270e8f549a1eba257ee709eb12c849031869d
GabrielBlanc0o <gtbvacation@outlook.es>
2026-06-09 10:21:23 -0500

    feat: implement Animal class and subclasses (Dog, Cat, Cow) with sound methods and food management

---
+++ 02-Object-Oriented/src/POO_ibm/Polimorfismo/my_poly_proj/src/Animal.java
@@ -0,0 +1,69 @@
+public class Animal
+{
+    private String name;
+    private String food;
+
+    public Animal(String name)
+    {
+        this.name = name;
+    }
+
+    public String sound()
+    {
+        return null;
+    }
+    public String toString()
+    {
+        return name.concat(" says ").concat(sound().concat(" and eats ").concat(food));
+    }
+
+    public void setFood(String food)
+    {
+        this.food = food;
+    }
+
+    public String getFood()
+    {
+        return this.food;
+    }
+}
+
+class Dog extends Animal
+{
+    public Dog(String name)
+    {
+        super(name);
+    }
+    public String sound()
+    {
+        return "Woof";
+    }
+}
+
+
+class Cat extends Animal
+{
+    public Cat(String name)
+    {
+        super(name);
+    }
+    public String sound()
+    {
+        return "Meow";
+    }
+}
+
+
+class Cow extends Animal
+{
+    public Cow(String name)
+    {
+        super(name);
+    }
+    public String sound()
+    {
+        return "Moo";
+    }
+}
+
+

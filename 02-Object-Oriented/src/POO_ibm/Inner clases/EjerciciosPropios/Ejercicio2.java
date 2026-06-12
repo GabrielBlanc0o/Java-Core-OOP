20cb3955fa5e2a47b13ce70394938a3eeb1c8a80
GabrielBlanc0o <gtbvacation@outlook.es>
2026-06-12 11:38:39 -0500

    FEAT: creacion desde cero de un patron de diseño iterador usando clases internas!!! exitoso

---
+++ 02-Object-Oriented/src/POO_ibm/Inner clases/EjerciciosPropios/Ejercicio2.java
@@ -0,0 +1,64 @@
+class ContenedorHistorial
+{
+    private Object[] registros;
+    private int indice = 0;
+
+    public ContenedorHistorial() {
+        registros = new Object[10];
+    }
+
+    void agregarRegistro(Object obj)
+    {
+        registros[indice++] = obj;
+    }
+
+    class Iterador 
+    {
+        private int posicionActual = 0;
+
+        boolean tieneSiguiente(){
+            if (posicionActual < indice)
+            {
+                return true;
+            }
+            else
+            {
+                    return false;
+            }
+        }
+
+        Object siguiente(){
+            return registros[posicionActual++];
+        }
+    }
+
+    void procesarConFormato()
+    {
+        class Formateador
+        {
+            String formatear(Object dato)
+            {
+                return "[LOG SECRETO] " + dato.toString();
+            }
+        }
+    }
+}
+
+public class Ejercicio2
+{
+    public static void main(String []args)
+    {
+        ContenedorHistorial contenedor = new ContenedorHistorial();
+
+
+        contenedor.agregarRegistro("Login Exitoso");
+        contenedor.agregarRegistro("Error 404");
+        contenedor.agregarRegistro("Clase interna Creada");
+        ContenedorHistorial.Iterador iterador = contenedor.new Iterador();
+
+        while (iterador.tieneSiguiente())
+        {
+            System.out.println(iterador.siguiente());
+        }
+    }
+}

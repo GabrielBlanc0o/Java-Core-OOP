6a206d7af84399c9de4c4ea4f528822c60227917
GabrielBlanc0o <gtbvacation@outlook.es>
2026-06-23 12:28:18 -0500

    Refactor WriteToFile to use try-with-resources and improve error handling

--- 02-Object-Oriented/src/POO_ibm/Administracion de archivos y Directorios/WriteToFile.java
+++ 02-Object-Oriented/src/POO_ibm/Administracion de archivos y Directorios/WriteToFile.java
@@ -8,16 +8,19 @@ import java.io.IOException;
 
 public class WriteToFile {
     public static void main(String []args) {
-        try {
-            FileWriter writer = new FileWriter("output.txt");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))){
+            writer.write("Hola Mundo");
+            
+           /*/ FileWriter writer = new FileWriter("output.txt");
             BufferedWriter bufferedWriter = new BufferedWriter(writer);
             bufferedWriter.write("Hola, mundo!");
             bufferedWriter.newLine();
             bufferedWriter.write("Esto es un ejemplo del manejo de un archivo en java ");
-            bufferedWriter.close();
+            */
             System.out.println("Datos correctamente escritos.");
         } catch (IOException e) {
-            System.out.println("Un error ocurrio: " + e.getMessage());
+             e.printStackTrace();
         }
+        
     }
 }

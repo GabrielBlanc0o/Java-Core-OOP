c20a386e77e7280c7a0c27a9d3547c8a52833468
GabrielBlanc0o <gtbvacation@outlook.es>
2026-06-27 12:08:34 -0500

    Add Ejercicio2 class with file handling methods for writing and reading data

---
+++ 02-Object-Oriented/src/POO_ibm/Administracion de archivos y Directorios/ByteStream/Ejemplos /Ejercicio2.java
@@ -0,0 +1,57 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+interface metodos {
+    void escribir();
+    void leer();
+}
+
+class opciones implements metodos {
+
+    public opciones() {}
+    
+    @Override
+    public void escribir() {
+        int contador = 1;
+        try(BufferedWriter archivo = new BufferedWriter(new FileWriter("notas.txt"))){
+            archivo.write(contador + ". Java");
+            archivo.newLine();
+            contador++;
+            archivo.write(contador + ". PostgreSQL");
+            archivo.newLine();
+            contador++;
+            archivo.write(contador + ". Python");
+        } catch (IOException e) {
+            System.out.println("Error " + e.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public void leer() {
+        try (BufferedReader leer = new BufferedReader(new FileReader("notas.txt"))){
+            String linea;
+            int contador = 1;
+            while((linea = leer.readLine()) != null) {
+                System.out.println(linea);
+                contador++;
+            }
+            System.out.println("Total lineas: " + (contador - 1));
+        } catch (IOException e) {
+            System.out.println("Error "  + e.getLocalizedMessage());
+        }
+    } 
+}
+
+public class Ejercicio2 {
+    public static void main(String []args) {
+
+        opciones menu = new opciones();
+        menu.escribir();
+        menu.leer();
+
+    }
+}

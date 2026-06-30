a9d2266dbaf20133c9cc00c2bcbaed0bb485edd7
GabrielBlanc0o <gtbvacation@outlook.es>
2026-06-30 18:17:44 -0500

    feat(JavaDateTimeClasses): add LocalDateExample and LocalDateTimeExample classes for date and time demonstration

---
+++ 02-Object-Oriented/src/POO_ibm/JavaDateTimeClasses/LocalDateExample.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+public class LocalDateExample {
+    public static void main(String[] args) {
+        LocalDate hoy = LocalDate.now();
+        System.out.println("Fecha del dia de hoy " + hoy)  ;
+    }   
+}
---
+++ 02-Object-Oriented/src/POO_ibm/JavaDateTimeClasses/LocalDateTimeExample.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+
+public class LocalDateTimeExample {
+    public static void main(String[] args) {
+        LocalDateTime ahora = LocalDateTime.now();
+        System.out.println("Fecha actual y tiempo: "+ ahora);
+    }    
+}
